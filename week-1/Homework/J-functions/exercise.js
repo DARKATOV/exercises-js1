040055ea81599931800717b09d019dc1780821f2
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-02-20 18:36:12 +0100

    actualizacion ejercicios de J

--- week-1/Homework/J-functions/exercise.js
+++ week-1/Homework/J-functions/exercise.js
@@ -1,7 +1,16 @@
 function halve(number) {
   // complete the function here
+  return number/2;
 }
 
 var result = halve(12);
+console.log(result);
 
+var result = halve(6);
+console.log(result);
+
+var result = halve(5);
+console.log(result);
+
+var result = halve(7.58858);
 console.log(result);
--- week-1/Homework/J-functions/exercise2.js
+++ week-1/Homework/J-functions/exercise2.js
@@ -1,7 +1,16 @@
 function triple(number) {
   // complete function here
+  return number*3;
 }
 
 var result = triple(12);
+console.log(result);
 
+var result = triple(777);
+console.log(result);
+
+var result = triple(1414);
+console.log(result);
+
+var result = triple(0.21);
 console.log(result);
