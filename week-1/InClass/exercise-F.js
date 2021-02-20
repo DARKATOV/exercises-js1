42c897315e4b6a2669ccdd73f4c48e8fbd89d4cf
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-02-20 14:21:05 +0100

    actualizacion F

---
+++ week-1/InClass/exercise-F.js
@@ -0,0 +1,18 @@
+let numberOfStudents = 20;
+let p1 ="El numero de estudiantes es: "
+
+const r1 = `${p1} ${numberOfStudents} `;
+
+let numberOfMentors = 2;
+let p2 ="El numero de mentores es: "
+
+const r2 = `${p2} ${numberOfMentors} `;
+
+const total = numberOfStudents + numberOfMentors
+let p3 ="El numero total de personas es: "
+
+const r3 = `${p3} ${total} `;
+
+console.log(r1);
+console.log(r2);
+console.log(r3);
