e1c6f459bdd6c572d87917d316484edb4e35d30e
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-02-20 14:04:41 +0100

    actualizacion

---
+++ week-1/InClass/exercise-D.js
@@ -0,0 +1,27 @@
+let colors = "blue,yellow";
+
+let colorsType = typeof colors
+
+console.log(colorsType);
+
+
+
+let colors2 = "red,yellow";
+
+console.log(typeof colors2);
+
+
+
+
+let number = 255;
+
+let numberType = typeof number
+
+console.log(numberType);
+
+
+
+let operador = true; 
+
+console.log(typeof operador);
+
