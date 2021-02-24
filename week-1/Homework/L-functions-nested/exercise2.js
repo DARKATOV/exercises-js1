1b144b40609b34821537a77fca4e085b8c0f8c4e
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-02-24 16:26:24 +0100

    actualizacion ejercecios de L

---
+++ week-1/Homework/L-functions-nested/exercise2.js
@@ -0,0 +1,24 @@
+
+
+function welcome(names)
+
+{
+    let names = names.toUpperCase();
+    return `HELLO ${mayus}`;
+}
+
+
+let mentors = welcome("Daniel");
+console.log(mentors);
+
+let mentors = welcome("Irina");
+console.log(mentors);
+
+let mentors = welcome("Mimi");
+console.log(mentors);
+
+let mentors = welcome("Rob"); 
+console.log(mentors);
+
+let mentors = welcome("Yohannes");
+console.log(mentors);
