d5439fb10f954dc04f61a9186d2256afbd421651
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-02-24 21:54:20 +0100

    actualizacion C y G

--- week-2/InClass/exercise-G.js
+++ week-2/InClass/exercise-G.js
@@ -1,4 +1,9 @@
-var apolloCountdownMessage = "all engine running... LIFT-OFF!";
-var countdown = 8;
 
-console.log(apolloCountdownMessage);
+let countdown = 8;
+
+while (countdown != 0)
+{ console.log(`all engine running ${countdown} LIFT-OFF!`);
+countdown = countdown - 1;  
+}
+
+
