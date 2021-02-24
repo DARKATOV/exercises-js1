e07655a524299303e69840f9b69d9c06640de2ac
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-02-24 15:28:53 +0100

    actualizacion de k-4

--- week-1/Homework/K-functions-parameters/exercise4.js
+++ week-1/Homework/K-functions-parameters/exercise4.js
@@ -2,4 +2,12 @@
 
 // Call the function and assign to a variable `sum`
 
+function operator(number1, number2)
+
+{
+    return number1 + number2;
+}
+
+let sum = operator (13, 124)
+
 console.log(sum);
