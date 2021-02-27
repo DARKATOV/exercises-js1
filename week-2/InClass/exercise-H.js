0429c9abb08601ecd141fc6d2aaf97eeaeb36439
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-02-27 15:52:40 +0100

    actualizacion ejercicios in class

--- week-2/InClass/exercise-H.js
+++ week-2/InClass/exercise-H.js
@@ -1,7 +1,20 @@
-function exponential(number) {
-  return number * number;
-}
 
-function isEven(number) {
-  return number % 2 === 0;
-}
+  function exponential(number) {
+    return number * number;
+  }
+
+  function isEven(number) {
+    return number % 2 === 0;
+  }
+
+  for (let number = 5; number <= 20; number += 1) {
+
+    if (isEven(number) === 0) {
+      console.log( `The exponencial of ${number} is ${exponential(number)}`);
+    }
+  }
+
+
+
+
+//el operador porcentaje es el operador de modulo 
--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,28 @@
-function secondMatchesAmy(array) {
-  if ( ) {
+function secondMatchesAmy (result) {
+  if ( names[1] === "Amy" ) {
     return "Second index matched!";
   }
+  
+  else {return "Second index not matched";
+  }
+
+  }
+
+function secondMatchesAmy2 (result2) {
+  if ( names2[1] === "Amy" ) {
+    return "Second index matched!";
+  }
+  else {
   return "Second index not matched";
+  }
 }
+
+
+let names = ["Alex", "Amara", "Carlos"];
+let names2 = ["Ali", "Amy", "Naresh"];
+
+const result = secondMatchesAmy(names);
+const result2 = secondMatchesAmy2(names2);
+
+console.log(result);
+console.log(result2);
