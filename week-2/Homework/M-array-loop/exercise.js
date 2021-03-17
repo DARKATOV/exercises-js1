e3e42fa183d51842a2d51eca6d5041ad811ff085
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-03-17 17:59:33 +0100

    act

--- week-2/Homework/M-array-loop/exercise.js
+++ week-2/Homework/M-array-loop/exercise.js
@@ -13,3 +13,18 @@ const daysOfWeek = [
   "Saturday",
   "Sunday",
 ];
+
+function array(arr){
+  let cuanti = arr.legth ; 
+  for (let i=0; i<=cuanti ; i ++ ) {
+    total
+    var start= arr[i].startWith('T');
+    if (start = true ) {
+      console.log(arr);
+    }; 
+    }
+}
+
+console.log(array(daysOfWeek));
+
+// preguntar duda 
