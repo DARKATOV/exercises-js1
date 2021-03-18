d637a429cd35a491e2f24643a31f7789727fb315
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-03-18 22:32:46 +0100

    finish

--- week-3/Homework/D-array-filter/exercise.js
+++ week-3/Homework/D-array-filter/exercise.js
@@ -6,17 +6,22 @@
   - Do not edit any of the existing code
 */
 
-var pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], null, [1], false, "whoops"];
+let pairsByIndexRaw = [[0, 3], [1, 2], [2, 1], 'null', [1], false, "whoops"];
 
-var pairsByIndex; // Complete this statement
 
-var students = ["Islam", "Lesley", "Harun", "Rukmini"];
-var mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
+let students = ["Islam", "Lesley", "Harun", "Rukmini"];
+let mentors = ["Daniel", "Irina", "Mozafar", "Luke"];
 
-var pairs = pairsByIndex.map(function(indexes) {
-  var student = students[indexes[0]];
-  var mentor = mentors[indexes[1]];
-  return [student, mentor];
+function filter(pair) {
+    return pair.length==2 && Array.isArray(pair);
+}
+
+let pairsByIndex = pairsByIndexRaw.filter(filter); // Complete this statement
+
+let pairs = pairsByIndex.map(function(indexes) {
+  let student = students[indexes[0]];
+  let mentor = mentors[indexes[1]];
+    return [student, mentor];
 });
 
 console.log(pairs);
